1c94c921359828befd21e329c9b02b3a038202f7
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-03-13 09:42:10 +0600

    firebase config and env file creating done

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,8 +1,9 @@
-
+import { Button } from "@/components/ui/button"
 const Home = () => {
     return (
         <div>
             <h1 className="text-2xl text-red-500">home page </h1>
+            <Button>Button</Button>
         </div>
     );
 };
