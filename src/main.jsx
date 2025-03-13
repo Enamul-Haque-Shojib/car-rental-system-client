07b5fe73a27eaa4a8136d8d7470f9d4b72cb0922
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-03-13 09:59:46 +0600

    added AuthProvider and useAuth hooks setup done

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import MainLayout from './layout/MainLayout.jsx'
 import Home from './page/home/Home.jsx'
+import AuthProvider from './Provider/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <RouterProvider router={router}></RouterProvider>
      */} <BrowserRouter>
-     <Routes>
-  <Route path="/" element={<MainLayout />}>
-    <Route index element={<Home />} />
-    
-  </Route>
-</Routes>
-</BrowserRouter>
+      <Routes>
+        <AuthProvider>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+
+          </Route>
+        </AuthProvider>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
